cbc1bc46f62b44078013a2a7ee0d01ba4f5a1612
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-04-11 17:00:54 +0200

    migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->text('bio');
+            $table->string('username')->unique();
+            $table->string('slug')->unique();
+            $table->string('avatar')->nullable();
+            $table->text('cv');
+            $table->integer('cellphone');
+            $table->string('address');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
