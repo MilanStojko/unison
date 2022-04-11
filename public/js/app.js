2b530d9ba763e6d86d2096ecce0cceb56b4d0486
SimoMassaro52 <simonesimomassaro@gmail.com>
2022-04-11 15:43:47 +0200

    push pacchetti

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\milan\Documents\Boolean\unison\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\milan\Documents\Boolean\unison\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\simon\Desktop\unison\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\simon\Desktop\unison\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
