9fa1b693440069225ae71bf3772513ce3ba1a618
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-04-12 18:07:49 +0200

    crud for users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Availability;
+use App\Category;
 use App\User;
 use App\Http\Controllers\Controller;
+use App\Sponsorship;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
+
+    protected $validation = [
+        'name' => 'required|string|max:255',
+        'surname' => 'required|string|max:25',
+        'bio' => 'required|text|max:255',
+        'username' => 'required|string|max:255|unique:users',
+        'avatar' => 'nullable|image',
+        'cv' => 'nullable|image',
+        'cellphone' => 'required|int',
+        'address' => 'nullable',
+        'categories' => 'nullable|exist:categories,id',
+        'availabilities' => 'nullable|exist:availabilities,id',
+        'sponsorships' => 'nullable|exist:sponsorships,id',
+        'email' => 'required|string|email|max:255|unique:users',
+    ];
+
+    protected function slug($title = "", $id = "")
+    {
+        $tmp = Str::slug($title);
+        $count = 1;
+        while (User::where('slug', $tmp)->where('id', '!=', $id)->first()) {
+            $tmp = Str::slug($title) . "-" . $count;
+            $count++;
+        }
+        return $tmp;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -58,9 +88,13 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        $categories = Category::all();
+        $availabilities = Availability::all();
+        $sponsorships = Sponsorship::all();
+
+        return view('admin.users.edit', compact('user', 'categories', 'availabilities', 'sponsorships'));
     }
 
     /**
