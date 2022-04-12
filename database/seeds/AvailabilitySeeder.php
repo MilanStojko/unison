e1a69d6ee1249c1be5c2021b7f6bb92c4c655215
SimoMassaro52 <simonesimomassaro@gmail.com>
2022-04-12 15:24:27 +0200

    seeded categories and availabilities

--- database/seeds/AvailabilitySeeder.php
+++ database/seeds/AvailabilitySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Availability;
 
 class AvailabilitySeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class AvailabilitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $availabilities = ['composizione', 'arrangiamento', 'direzione orchestrale', 'direzione coro', 'cerimonie', 'DJ set', 'didattica', 'canto', 'performance', 'improvvisazione', 'direzione di ensemble', 'virtuosismo', 'polistrumentismo', 'coro religioso', 'cerimonie religiose', 'repertorio', 'banda', 'complesso strumentale', 'complesso vocale', 'cantautorato', 'musica colta', 'folklore', 'musica lirica', 'turnista', 'concerto', 'jam session', 'membro', 'sideman', 'cover', 'festival'];
+        foreach ($availabilities as $availability_name) {
+            $new_availability = new Availability();
+            $new_availability->name = $availability_name;
+            $new_availability->save();
+        }
     }
 }
--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['jazzista', 'arrangiatore', 'bassista', 'violoncellista', 'direttore di coro', 'direttore di orchestra', 'DJ', 'insegnante', 'cantante', 'performer', 'virtuoso', 'polistrumentista', 'corista', 'corista religioso', 'membro banda', 'vocalist', 'cantautore', 'soprano', 'tenore', 'chitarrista classico', 'chitarrista folk', 'chitarrista rock'];
+        foreach ($categories as $category_name) {
+            $new_category = new Category();
+            $new_category->name = $category_name;
+            $new_category->save();
+        }
     }
 }
