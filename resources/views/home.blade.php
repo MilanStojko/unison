46e1e12fd59bb24af7f595920b1d03beec66d9e5
SimoMassaro52 <simonesimomassaro@gmail.com>
2022-04-12 15:56:39 +0200

    partial dashboard implementation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.menu')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-header">La tua dashboard</div>
+                <div class="card-content">
+                    <h1></h1>
                 </div>
             </div>
         </div>
