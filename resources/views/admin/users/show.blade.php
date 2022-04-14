3d8636be8ac884ff096e74e38e9a011b34cddaef
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-04-14 17:22:08 +0200

    test show

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -5,9 +5,20 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">La tua dashboard</div>
-                    <div class="card-content">
-                        <h1></h1>
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <div><h3>Admin / La tua dashboard</h3></div>
+                        <div class="card-head_content d-flex justify-content-between align-items-center">
+                            <img src="">
+                            <h3>
+                                {{$user->name}} B
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card-content p-4">
+
+                        <div class="profile-image">
+                            
+                        </div>
                     </div>
                 </div>
             </div>
