3819a0dd70cf8085c4f237002c304743a8f91adc
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-04-14 12:09:43 +0200

    v0.0.2

--- resources/js/guest/components/Header.vue
+++ resources/js/guest/components/Header.vue
@@ -1,121 +1,127 @@
 <template>
-    <nav>
-        <div class="logo">
-            UNISON
-        </div>
-        <ul class="my_ul">
-            <li v-for="(voice, index) in link" :key="index"><a class="my_a" href="#"><p>{{voice.name}}</p></a></li>
-            <li><a class="my_a login" href="#"><p class="my_button">Login</p></a></li>
-        </ul>
-        <ul class="my_ul-icon">
-            <li><a class="my_a" href="#">icon</a></li>
-        </ul>
-    </nav>
+  <nav>
+    <div class="logo">UNISON</div>
+    <ul class="my_ul">
+      <li v-for="(voice, index) in link" :key="index">
+        <a class="my_a" href="#"
+          ><p>{{ voice.name }}</p></a
+        >
+      </li>
+      <li>
+        <a class="my_a login" href="http://127.0.0.1:8000/login"
+          ><p class="my_button">Login</p></a
+        >
+      </li>
+    </ul>
+    <ul class="my_ul-icon">
+      <li><a class="my_a" href="#">icon</a></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: "Header",
+  name: "Header",
 
-    data() {
-        return {
-            link: [
-                {
-                    name: "Cerca",
-                },
-                {
-                    name: "Categorie",
-                },
-                {
-                    name: "Eventi",
-                },
-            ],
-        };
-    },
+  data() {
+    return {
+      link: [
+        {
+          name: "Cerca",
+        },
+        {
+          name: "Categorie",
+        },
+        {
+          name: "Eventi",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo{
-    font-size: 30px;
+.logo {
+  font-size: 30px;
 }
 
-nav{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: rgb(42, 41, 41);
-    height: 50px;
-    padding: 0px 20px;
-    color: white;
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(42, 41, 41);
+  height: 50px;
+  padding: 0px 20px;
+  color: white;
 }
 
-.my_ul{
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
+.my_ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
 }
 
-li{
-    float: left;
-    padding: 0px 15px;
-    list-style: none;
+li {
+  float: left;
+  padding: 0px 15px;
+  list-style: none;
 }
 
-.my_a{
-    text-decoration: none;
-    color: white;
+.my_a {
+  text-decoration: none;
+  color: white;
 }
 
-.my_ul .my_a p{
-    transition: 1s;
+.my_ul .my_a p {
+  transition: 1s;
 }
 
-.my_ul .my_a p:hover{
-    color: white;
-    transform: scale(1.3);
+.my_ul .my_a p:hover {
+  color: white;
+  transform: scale(1.3);
 }
 
-.my_ul .my_a:hover .my_button{
-    color: white;
-    transform: scale(1.1);
+.my_ul .my_a:hover .my_button {
+  color: white;
+  transform: scale(1.1);
 }
 
-.navbar-toggler-icon, .navbar-toggler, .nav-item p{
-    color: white;
+.navbar-toggler-icon,
+.navbar-toggler,
+.nav-item p {
+  color: white;
 }
 
-.my_button{
-    padding: 5px 15px;
-    border: 0;
-    color: white;
-    background: #527A5A;
-    border-radius: 20px;
-    margin-left: 40px;
+.my_button {
+  padding: 5px 15px;
+  border: 0;
+  color: white;
+  background: #527a5a;
+  border-radius: 20px;
+  margin-left: 40px;
 }
 
+.my_ul-icon {
+  display: none;
+  margin: 0;
+  padding: 0;
+}
 
+@media only screen and (max-width: 700px) {
+  .my_ul-icon {
+    display: block;
+  }
 
-.my_ul-icon{
+  .my_ul {
     display: none;
-    margin: 0;
-    padding: 0;
-}
-
-@media only screen and (max-width: 700px){
-    .my_ul-icon{
-        display: block;
-    }
-
-    .my_ul{
-        display: none;
-    }
+  }
 }
 </style>
