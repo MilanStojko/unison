5211e5db4f2a3e2b29e38e4a3af3ea9a6cbf8774
francesco-abritta <fra.abritta@gmail.com>
2022-04-19 11:51:50 +0200

    nav front

--- resources/js/guest/components/Header.vue
+++ resources/js/guest/components/Header.vue
@@ -1,121 +1,252 @@
 <template>
-    <nav>
-        <div class="logo">
-            UNISON
-        </div>
-        <ul class="my_ul">
-            <li v-for="(voice, index) in link" :key="index"><a class="my_a" href="#"><p>{{voice.name}}</p></a></li>
-            <li><a class="my_a login" href="#"><p class="my_button">Login</p></a></li>
-        </ul>
-        <ul class="my_ul-icon">
-            <li><a class="my_a" href="#">icon</a></li>
-        </ul>
-    </nav>
+<div>
+  <nav>
+    <div class="logo">UNISON</div>
+    <ul class="my_ul">
+      <li v-for="(voice, index) in link" :key="index">
+        <a class="my_a" href="#"><p>{{ voice.name }}</p></a>
+      </li>
+      <li>
+        <a class="my_a login" href="http://127.0.0.1:8000/login"><p class="my_button">Login</p></a>
+      </li>
+    </ul>
+    <ul class="my_ul-icon">
+      <li>
+        <input @click="show(click)" type="checkbox" id="menyAvPaa">
+        <label id="burger" for="menyAvPaa">
+            <div></div>
+            <div></div>
+            <div></div>
+        </label>
+      </li>
+    </ul>
+  </nav>
+  <Transition name="slide-fade">
+  <div class="listmenu" v-show="click==1">
+      <ul>
+        <li v-for="(voice, indice) in link" :key="indice">
+          <a class="my_a_mobile" href="#"><span>{{ voice.name }}</span></a>
+        </li>
+        <li class=buttonlogin>
+          <a class="my_a login" href="http://127.0.0.1:8000/login"><span class="my_button">Login</span></a>
+        </li>
+      </ul>
+  </div>
+  </Transition>
+</div>
+
+  
 </template>
 
 <script>
 export default {
-    name: "Header",
+  name: "Header",
 
-    data() {
-        return {
-            link: [
-                {
-                    name: "Cerca",
-                },
-                {
-                    name: "Categorie",
-                },
-                {
-                    name: "Eventi",
-                },
-            ],
-        };
+  data() {
+    return {
+      click: 0,
+      link: [
+        {
+          name: "Cerca",
+        },
+        {
+          name: "Categorie",
+        },
+        {
+          name: "Eventi",
+        },
+      ],
+    };
+  },
+  methods:{
+    show: function(click){
+      this.click=!click;
     },
+  }
 };
 </script>
 
 <style scoped>
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
 
-.logo{
-    font-size: 30px;
-}
 
-nav{
+.burger-wrapper {
+  cursor: pointer;
+}
+#menyAvPaa {
+    display: none;
+}
+#burger {
+    position: absolute;
+    cursor: pointer;
+    width: 2rem;
+    height: 1.5rem;
+    right: 1rem;
+    top: 1rem;
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    background-color: rgb(42, 41, 41);
-    height: 50px;
-    padding: 0px 20px;
-    color: white;
+    flex-direction: column;
 }
 
-.my_ul{
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
+#burger > div {
+    height: 4px;
+    background-color: white;
+    transition: 0.5s;
+    z-index: 999;
 }
 
-li{
-    float: left;
-    padding: 0px 15px;
-    list-style: none;
+#menyAvPaa:checked ~ #burger > div {
+  background-color: #fff;
 }
 
-.my_a{
-    text-decoration: none;
-    color: white;
+#menyAvPaa:checked ~ #burger > div:nth-child(1) {
+    transform: translateY(10px) rotate(45deg);
 }
-
-.my_ul .my_a p{
-    transition: 1s;
+#menyAvPaa:checked ~ #burger > div:nth-child(2) {
+    opacity: 0;
 }
-
-.my_ul .my_a p:hover{
-    color: white;
-    transform: scale(1.3);
-}
-
-.my_ul .my_a:hover .my_button{
-    color: white;
-    transform: scale(1.1);
-}
-
-.navbar-toggler-icon, .navbar-toggler, .nav-item p{
-    color: white;
-}
-
-.my_button{
-    padding: 5px 15px;
-    border: 0;
-    color: white;
-    background: #527A5A;
-    border-radius: 20px;
-    margin-left: 40px;
+#menyAvPaa:checked ~ #burger > div:nth-child(3) {
+    transform: translateY(-10px) rotate(-45deg);
 }
 
 
 
-.my_ul-icon{
+
+
+
+
+
+
+.slide-fade-enter-active {
+  transition: opacity 1.5s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+
+
+
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.logo {
+  font-size: 30px;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(42, 41, 41);
+  height: 50px;
+  padding: 0px 20px;
+  color: white;
+  position: relative;
+}
+
+.my_ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+}
+
+.my_ul li {
+  float: left;
+  padding: 0px 15px;
+  list-style: none;
+}
+
+.my_a {
+  text-decoration: none;
+  color: white;
+}
+
+.my_ul .my_a p {
+  transition: 1s;
+}
+
+.my_ul .my_a p:hover {
+  color: white;
+  transform: scale(1.3);
+}
+
+.my_ul .my_a:hover .my_button {
+  color: white;
+  transform: scale(1.1);
+}
+
+.navbar-toggler-icon,
+.navbar-toggler,
+.nav-item p {
+  color: white;
+}
+
+.my_button {
+  padding: 5px 15px;
+  border: 0;
+  color: white;
+  background: #527a5a;
+  border-radius: 15px;
+}
+
+.my_ul-icon {
+  display: none;
+  margin: 0;
+  padding: 0;
+}
+
+.my_ul-icon li{
+  list-style: none;
+}
+
+.listmenu{
+  position: absolute;
+  right: 0;
+  z-index: 999;
+  padding: 20px;
+  width: 100%;
+  height: calc(100vh - 50px);
+  text-align: center;
+  background: rgb(255, 255, 255, 0.9);
+  display: flex;
+  flex-direction: column;
+}
+
+.listmenu li{
+  line-height: 100px;
+  font-weight: bold;
+  font-size: 40px;
+  list-style: none;
+}
+
+.listmenu .buttonlogin{
+  margin-top: 50px;
+}
+
+.my_a_mobile {
+  text-decoration: none;
+  color: black;
+}
+
+@media only screen and (max-width: 700px) {
+  .my_ul-icon {
+    display: block;
+  }
+
+  .my_ul {
     display: none;
-    margin: 0;
-    padding: 0;
-}
-
-@media only screen and (max-width: 700px){
-    .my_ul-icon{
-        display: block;
-    }
-
-    .my_ul{
-        display: none;
-    }
+  }
 }
 </style>
