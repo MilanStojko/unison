02e6e05b429663d588636a673bb057282e093262
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-04-21 17:28:44 +0200

    API for avg vote

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,4 +22,25 @@ class UserController extends Controller
         $user = User::where('slug', $slug)->with('categories', 'availabilities', 'sponsorships', 'reviews')->first();
         return response()->json($user);
     }
+
+    public function getCountReview()
+    {
+        $user = User::join("reviews", "user_id", "=", "reviews.user_id")
+            ->select(array('users.*', DB::raw('COUNT(`user_id`) as num_rev')))
+            ->groupBy(DB::raw("CONVERT(users.id, CHAR)"), 'users.id')
+            ->orderBy('users.id', 'desc')
+            ->get();
+        return response()->json($user);
+    }
+
+    public function getAvgVote($minvote)
+    {
+        $user = User::join("reviews", "user_id", "=", "reviews.user_id")
+            ->select(array('users.*', DB::raw('AVG(`vote`) as avg_vote')))
+            ->groupBy(DB::raw("CONVERT(users.id, CHAR)"), 'users.id')
+            ->havingRaw("avg_vote BETWEEN ? AND ?", [$minvote, 5])
+            ->orderBy('users.id', 'desc')
+            ->get();
+        return response()->json($user);
+    }
 }
